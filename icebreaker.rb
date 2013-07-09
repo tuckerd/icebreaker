1fb3a57cd342d9cd991a745b5de70b928bee171c
Nathalie Steinmetz <nathalie.steinmetz@gmail.com>
2013-07-08 22:20:54 -0700

    More icebreaker questions

--- icebreaker.rb
+++ icebreaker.rb
@@ -49,6 +49,9 @@ questions = [ "What is your favorite food?",
   "Which is your favorite city and/or country you've traveled to?",
   "Tell us one goal that you'd like to achieve this year?"
   "What is the most amazing book you've read, and why?"
-  "If you could travel to any country, without restrictions or security issues, where would you go and why?" 
-  "Are you a dog person or a cat person? Or none?"]
+  "If you could travel to any country, without restrictions or safety/security issues, where would you go and why?" 
+  "Are you a dog person or a cat person? Or none?"
+  "How do you drink your coffee?" 
+  "What is your favorite communication medium"
+  "Assuming you don't work at home, what is your favorite means of transportation to get to work?" ]
 puts questions.sample
